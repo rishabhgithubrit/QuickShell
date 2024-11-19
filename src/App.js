03fea9e993abc73c6509f64c6899a1315368fbf2
RISHABH NIGAM <119465076+rishabhgithubrit@users.noreply.github.com>
2024-11-20 00:07:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 // import Card from './components/Card/Card';
 import DashBoard from './components/DashBoard/DashBoard';
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchAllData } from './Actions/DataAction';
 import Loading from './components/Loading/Loading';
+
 const App = () => {
   const dispatch = useDispatch();
-  const { allTickets } = useSelector(state => state.DataReducer);
+  const { allTickets, loading, error } = useSelector((state) => state.DataReducer);
+
   useEffect(() => {
     dispatch(fetchAllData());
-  }, [dispatch])
+  }, [dispatch]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <div style={{ color: 'red', textAlign: 'center' }}>Error: {error.message || "Failed to load data"}</div>;
+  }
+
   return (
-    <div style={{ paddingTop: "10px" }} >
+    <div style={{ paddingTop: '10px' }}>
       <NavBar />
-      <hr style={{ marginTop: "10px" }} />
-      <DashBoard />
+      <hr style={{ marginTop: '10px' }} />
+      <DashBoard allTickets={allTickets} />
     </div>
-  ) 
-}
-export default App
+  );
+};
+
+export default App;
